ca8e8294148f0749c68052d504ab788ed19cf711
mike-french <michaeltfrench@netscape.net>
2015-06-07 16:41:18 +0300

    reverse tests and nonempty generators

--- src/yazl_test.erl
+++ src/yazl_test.erl
@@ -31,69 +31,171 @@
           to_list/1,
           truncate/2
          ] ).
-
+         
 -include_lib("proper/include/proper.hrl").
+
+% ---------------------------------------------------
+% Proper utility types and generators
+
+% -type yazls :: yasl:yazl( integer() ).
+% -type yazls :: { list(integer()), list(integer()) }.
+
+yazls() -> { list(integer()), list(integer()) }.
+
+nonempty_yazls() -> { non_empty(list(integer())), 
+                      non_empty(list(integer())) }.
+
+% ---------------------------------------------------
+% Local utility functions
+
+reflect( r,  _, endr ) -> 1;
+reflect( l,  N, endl ) -> N;
+reflect( r,  _, 1    ) -> endr;
+reflect( l,  N, N    ) -> endl;
+reflect( r,  N, IR   ) -> N - IR + 2;
+reflect( l,  N, IL   ) -> N - IL.
+
+% ---------------------------------------------------
+% reverse( yazl(A) ) -> yazl(A).
+
+prop_reverse_empty() ->
+  io:fwrite( "Reversing empty yazl leaves yazl unchanged~n" ),
+  new() =:= reverse( new() ).
+
+prop_reverse_single() ->
+  io:fwrite( "Reversing a singleton leaves list unchanged~n" ),
+  L = [a],
+  L =:= to_list( reverse( from_list(L) ) ).
+
+prop_reverse_list() ->
+  io:fwrite( "Reversing yazl reverses the underlying list~n" ),     
+  ?FORALL( Z, yazls(),
+     lists:reverse( to_list(Z) ) =:= to_list( reverse(Z) )
+  ).
+  
+prop_reverse_twice_identity() ->
+  io:fwrite( "Reversing twice restores the original~n" ),     
+  ?FORALL( Z, yazls(),
+     Z =:= reverse( reverse(Z) )
+  ).
+  
+prop_reverse_switches_ends() ->
+  io:fwrite( "Reversing an end position switches the ending~n" ),
+  ?FORALL( L, list(),
+     (endr =:= position( r, reverse( from_list(endl,L) ) )) and
+     (endl =:= position( l, reverse( from_list(endr,L) ) ))
+  ).
+  
+prop_reverse_switches_current_values() ->
+  io:fwrite( "Reversing switches local values~n" ),
+  ?FORALL( Z, nonempty_yazls(),
+     begin
+       LVal = get( l, Z ),
+       RVal = get( r, Z ),
+       RevZ = reverse( Z ),
+       (LVal =:= get( r, RevZ )) and 
+       (RVal =:= get( l, RevZ ))
+     end
+  ).
+  
+prop_reverse_reflects_index() ->
+  io:fwrite( "Reversing reflects the index position~n" ),     
+  ?FORALL( Z, nonempty_yazls(),
+     begin
+       N = size( Z ),
+       PosR = position( r, Z ),
+       PosL = position( l, Z ),
+       RevZ = reverse( Z ),
+       PosRevR = position( r, RevZ ),
+       PosRevL = position( l, RevZ ),
+       (PosRevR =:= reflect( r, N, PosR )) and
+       (PosRevL =:= reflect( l, N, PosL ))
+     end
+  ).
   
 % ---------------------------------------------------
 % new() -> empty_yazl().
+% is_yazl( term() ) -> boolean().
 
 prop_new_value() ->
-  % an empty yazl is just a tuple of 2 empty lists
-  equals( new(), {[],[]} ).
+  io:fwrite( "An empty yazl is a tuple of 2 empty lists~n" ),
+  Z = new(),
+  is_yazl(Z) and (Z =:= {[],[]}).
+  
+prop_to_list_empty() ->
+  io:fwrite( "An empty yazl gives the empty list~n" ),
+  Z = new(),
+  is_yazl(Z) and ([] =:= to_list(Z)). 
 
 % ---------------------------------------------------
 % from_list( [A] ) -> yazl(A).
+% to_list( yazl(A) ) -> [A].
 
 prop_from_list_to_list() ->
-  % from and to list should return the original list
-  ?FORALL( L, list(), 
-    L =:= to_list( from_list(L) )
+  io:fwrite( "Roundtrip from list gives original list~n" ),
+  ?FORALL( L, list(),
+    begin
+      Z = from_list( L ),
+      is_yazl(Z) and (L =:= to_list(Z))
+    end
   ).
   
 % ---------------------------------------------------
 % from_list( position(), [A] ) -> yazl(A).
 
 prop_from_list_i_to_list() ->
-  % from list with position back to list 
-  % should return original list
-  ?FORALL( {L,I}, {list(),integer()},
-     ?IMPLIES( 
-        (1 =< I) and (I =< length(L)),
-        L =:= to_list( from_list(I,L) )
-     )
+  io:fwrite( "Roundtrip from list with position gives original list~n" ),
+  ?FORALL( L, non_empty(list()),
+     begin
+       N = length(L),
+       ?FORALL( I, range(1,N),
+          begin
+            Z = from_list(I,L),
+            is_yazl(Z) and (L =:= to_list(Z))
+          end
+       )
+     end
   ).
   
 % ---------------------------------------------------
-% is_yazl( term() ) -> boolean().
-
-% ---------------------------------------------------
 % is_empty( yazl(_) ) -> boolean().
 
+prop_is_empty_new() ->
+  io:fwrite( "An empty yazl is empty~n" ),
+  is_empty( new() ).
+  
+prop_is_empty_not() ->
+  io:fwrite( "Non-empty lists are non-empty~n" ),
+  ?FORALL( L, non_empty(list()), 
+     not is_empty( from_list(L) )
+  ).
+  
 % ---------------------------------------------------
 % size( yazl(_) ) -> non_neg_integer().
 
 prop_size_list() ->
+  io:fwrite( "Size is same as length of the list~n" ),
   ?FORALL( L, list(), 
-           length(L) =:= yazl:size( yazl:from_list(L) )
+     length(L) =:= size( from_list(L) ) 
   ).
 
 % ---------------------------------------------------
 % position( direction(), yazl(_) ) -> position().
 
 prop_position_from_endl() ->
-  % from list with position endl
-  ?FORALL( {L}, {list()},
+  io:fwrite( "Import list at endl makes positions l->endl and r->1|endr~n" ),
+  ?FORALL( L, list(),
      begin
        Z = from_list( endl, L ),
-       RPos = case size(Z) of 0 -> endr; _N -> 1 end,
+       RPos = case size(Z) of 0 -> endr; _N -> 1 end, 
        (endl =:= position( l, Z )) and
        (RPos =:= position( r, Z ))
      end
   ).
 
 prop_position_from_endr() ->
-  % from list with position endr
-  ?FORALL( {L}, {list()},
+  io:fwrite( "Import list at endr makes positions r->endr and l->1|endl~n" ),
+  ?FORALL( L, list(),
      begin
        Z = from_list( endr, L ),
        LPos = case size(Z) of 0 -> endl; N -> N end,
@@ -103,23 +205,55 @@ prop_position_from_endr() ->
   ).
   
 prop_position_from_i() ->
-  % from list with position i
-  ?FORALL( {L,I}, {list(),integer()},
-     ?IMPLIES( 
-        (1 =< I) andalso (I =< length(L)),
-        I =:= position( r, from_list(I,L) )
-     )
+  io:fwrite( "Import list at i has position r->i~n" ),
+  ?FORALL( L, non_empty(list()),
+     begin
+       N = length(L),
+       ?FORALL( I, range(1,N),
+          I =:= position( r, from_list(I,L) )
+       )
+     end
   ).
-
-% ---------------------------------------------------
-% to_list( yazl(A) ) -> [A].
 
 % ---------------------------------------------------
 % get( direction(), yazl(A) ) -> maybe(A).
 
 % ---------------------------------------------------
 % move( direction(), yazl(A) ) -> maybe(yazl(A)).
+-ifdef( ZZZ ).
+prop_position_from_endl() ->
+  io:fwrite( "Import list at endl makes positions l->endl and r->1|endr~n" ),
+  ?FORALL( L, list(),
+     begin
+       Z = from_list( endl, L ),
+       RPos = case size(Z) of 0 -> endr; _N -> 1 end, 
+       (endl =:= position( l, Z )) and
+       (RPos =:= position( r, Z ))
+     end
+  ).
 
+prop_position_from_endr() ->
+  io:fwrite( "Import list at endr makes positions r->endr and l->1|endl~n" ),
+  ?FORALL( L, list(),
+     begin
+       Z = from_list( endr, L ),
+       LPos = case size(Z) of 0 -> endl; N -> N end,
+       (endr =:= position( r, Z )) and
+       (LPos =:= position( l, Z ))
+     end
+  ).
+  
+prop_position_from_i() ->
+  io:fwrite( "Import list at i has position r->i~n" ),
+  ?FORALL( L, non_empty(list()),
+     begin
+       N = length(L),
+       ?FORALL( I, range(1,N),
+          I =:= position( r, from_list(I,L) )
+       )
+     end
+  ).
+-endif.
 % ---------------------------------------------------
 % moves( direction(), integer(), yazl(A) ) -> maybe(yazl(A)).
 
@@ -153,8 +287,7 @@ prop_position_from_i() ->
 % ---------------------------------------------------
 % truncate( direction(), yazl(A) ) -> yazl(A).
 
-% ---------------------------------------------------
-% reverse( yazl(A) ) -> yazl(A).
+
 
 % ---------------------------------------------------
 % map( direction(), fun((A)->B), yazl(A) ) -> [B].
